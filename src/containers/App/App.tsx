518a7a739396b52fc3476304c814e435c9ffb60d
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-15 00:10:48 +0600

    Added Alert to App Made call of Alert on Continue btn in ModalFooter

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,32 +1,44 @@
 import React, { useState } from "react";
+import Alert from "../../components/Alert/Alert";
 import Modal from "../../components/Modal/Modal";
 import ModalBody from "../../components/Modal/ModalBody";
 import ModalFooter from "../../components/Modal/ModalFooter";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
   const cancelModal = () => setShowModal(false);
   const openModal = () => setShowModal(true);
+
+  const cancelAlert = () => setShowAlert(false);
+  const openAlert = () => setShowAlert(true);
 
   const btnConfigurations = [
     {
       type: "primary",
       label: "Continue",
-      onClick: () => alert("Hello, world!"),
+      onClick: () => {
+        cancelModal();
+        openAlert();
+      },
     },
-    { type: "danger", label: "Close", onClick: cancelModal },
+    { type: "danger", label: "Close", onClick: () => cancelModal },
   ];
 
   return (
-    <React.Fragment>
-      <button className="btn btn-primary" onClick={openModal}>
+    <div className="container">
+      <button className="btn btn-primary my-5" onClick={openModal}>
         Open modal
       </button>
+      <Alert show={showAlert} type="success" onDismiss={cancelAlert} clickDismissable>
+        You clicked Continue
+      </Alert>
       <Modal title="Modal" show={showModal} onClose={cancelModal}>
         <ModalBody>This modal's content</ModalBody>
         <ModalFooter buttons={btnConfigurations} />
       </Modal>
-    </React.Fragment>
+    </div>
   );
 }
 
