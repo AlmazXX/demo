603a3652bee2af452dc806d83f2372ba51586ec3
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-14 21:36:23 +0600

    Installed bootstrap library

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../../components/Modal/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
+  const cancel = () => {setShowModal(false)}
   return (
     <React.Fragment>
-      
+      <Modal title="Modal" show={showModal} onClose={cancel}></Modal>
     </React.Fragment>
   );
 }
