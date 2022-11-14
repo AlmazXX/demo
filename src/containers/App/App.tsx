d048d26dccd7e07c3516ad5c12c6180e534ebfda
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-14 23:17:29 +0600

    Added Modal, ModalBody, ModalFooter components Added cancelModal, openModal functions Added showModal state Added btnConfigurations array

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
-import Modal from '../../components/Modal/Modal';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import Modal from "../../components/Modal/Modal";
+import ModalBody from "../../components/Modal/ModalBody";
+import ModalFooter from "../../components/Modal/ModalFooter";
 
 function App() {
-  const [showModal, setShowModal] = useState(true);
-  const cancel = () => {setShowModal(false)}
+  const [showModal, setShowModal] = useState(false);
+  const cancelModal = () => setShowModal(false);
+  const openModal = () => setShowModal(true);
+
+  const btnConfigurations = [
+    {
+      type: "primary",
+      label: "Continue",
+      onClick: () => alert("Hello, world!"),
+    },
+    { type: "danger", label: "Close", onClick: cancelModal },
+  ];
+
   return (
     <React.Fragment>
-      <Modal title="Modal" show={showModal} onClose={cancel}></Modal>
+      <button className="btn btn-primary" onClick={openModal}>
+        Open modal
+      </button>
+      <Modal title="Modal" show={showModal} onClose={cancelModal}>
+        <ModalBody>This modal's content</ModalBody>
+        <ModalFooter buttons={btnConfigurations} />
+      </Modal>
     </React.Fragment>
   );
 }
